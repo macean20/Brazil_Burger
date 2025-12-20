537b4f94128aa2e0379a7831e7945ab7a5d53798
macean-dev <maceanchristopher7@gmail.com>
2025-12-20 22:28:06 +0000

    feat(core): add helper utilities

---
+++ Helpers/SessionHelper.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace BrasilBurger.Helpers;
+
+public static class SessionHelper
+{
+    public static void SetObjectAsJson(this ISession session, string key, object value)
+    {
+        session.SetString(key, JsonConvert.SerializeObject(value));
+    }
+
+    public static T? GetObjectFromJson<T>(this ISession session, string key)
+    {
+        var value = session.GetString(key);
+        return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+    }
+}
