2bf8124a074156c0bd2fb1fdce910e053b685d89
macean-dev <maceanchristopher7@gmail.com>
2025-12-20 22:28:35 +0000

    feat(ui): add MVC views and static assets

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    const toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl).show();
+    });
+});
